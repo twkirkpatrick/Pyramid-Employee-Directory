fc5c933ad37e7e1aa4d18ca620b243f1a4d43c39
Tanner Kirkpatrick <twk4491@gmail.com>
2021-01-11 19:36:52 -0500

    table built and data populating

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 import UserItem from "./UserItem";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+});
 
 const Results = () => {
+  const classes = useStyles();
+
   /* const [filter, setFilter] = useState(""); */
   const [users, setUsers] = useState([]);
 
   const getUsers = async () => {
     const res = await axios.get(
-      "https://randomuser.me/api/?results=40&nat=us&inc=name,email,dob,phone,id,picture"
+      "https://randomuser.me/api/?results=100&nat=us&inc=name,email,dob,phone,id,picture"
     );
     setUsers(res.data.results);
   };
@@ -17,19 +34,35 @@ const Results = () => {
     getUsers();
   }, []);
   return (
-    <div>
-      {users.map((user) => (
-        <UserItem
-          firstName={user.name.first}
-          lastName={user.name.last}
-          email={user.email}
-          dob={user.dob.date}
-          phone={user.phone}
-          key={user.id.value}
-          image={user.picture.thumbnail}
-        />
-      ))}
-    </div>
+    <Container maxWidth="md">
+      <TableContainer component={Paper} style={{ marginTop: "2rem" }}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Image</TableCell>
+              <TableCell align="center">First</TableCell>
+              <TableCell align="center">Last</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">Phone</TableCell>
+              <TableCell align="center">DOB</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <UserItem
+                firstName={user.name.first}
+                lastName={user.name.last}
+                email={user.email}
+                dob={user.dob.date}
+                phone={user.phone}
+                key={user.id.value}
+                image={user.picture.thumbnail}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
 
-const UserItem = ({ firstName, lastName, email, dob, phone, picture }) => {
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+});
+
+const UserItem = ({ firstName, lastName, email, dob, phone, image }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>{`${firstName} ${lastName}`}</h1>
-      <ul>
-        <li>{email}</li>
-        <li>{dob}</li>
-        <li>{phone}</li>
-      </ul>
-    </div>
+    <TableRow>
+      <TableCell component="th" scope="row">
+        <img src={image} alt="user" />
+      </TableCell>
+      <TableCell align="center">{firstName}</TableCell>
+      <TableCell align="center">{lastName}</TableCell>
+      <TableCell align="center">{email}</TableCell>
+      <TableCell align="center">{phone}</TableCell>
+      <TableCell align="center">{dob}</TableCell>
+    </TableRow>
   );
 };
 
